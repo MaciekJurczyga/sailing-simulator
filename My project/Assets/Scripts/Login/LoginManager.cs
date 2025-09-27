f7bcf3896c136cc8503a8791979f357d6031fde5
Maciej <maciekjur123@gmail.com>
2025-09-27 19:02:01 +0200

    added login screen and login logic

---
+++ My project/Assets/Scripts/Login/LoginManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro; 
+
+public class LoginManager : MonoBehaviour
+{
+    public TMP_InputField loginInput;
+    public TMP_InputField passwordInput;
+    public Button loginButton;
+    public TextMeshProUGUI feedbackText; 
+    
+    public string mainSceneName = "Sailboat"; 
+
+    void Start()
+    {
+        loginButton.onClick.AddListener(HandleLogin);
+    }
+
+    private void HandleLogin()
+    {
+        String passwordHash = "fn9rzfGELzwC0NJ+3UWL031j0F+8B4A3od5TEI0Xt98=";
+        String loginHash = "jGl25bVBBBW96Qi9Te4V37Fnqchz/Eu4qB9vKrRIqRg=";
+        string login = loginInput.text;
+        string password = passwordInput.text;
+        
+        if (Sha256(login) == loginHash && Sha256(password) == passwordHash)
+        {
+            if (feedbackText != null)
+            {
+                feedbackText.text = "Login successful";
+                feedbackText.color = Color.green;
+            }
+            
+            SceneManager.LoadScene(mainSceneName);
+        }
+        else
+        {
+            passwordInput.text = "";
+            loginInput.text = "";
+            if (feedbackText != null)
+            {
+                feedbackText.text = "Incorrect login or password";
+                feedbackText.color = Color.red;
+            }
+        }
+    }
+    private static string Sha256(string s)
+    {
+        using (var sha = SHA256.Create())
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(s);
+            byte[] hash = sha.ComputeHash(bytes);
+            return Convert.ToBase64String(hash);
+        }
+    }
+    
+}
