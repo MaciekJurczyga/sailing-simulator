f412923750807b9ba3a370cc3439b0127133b490
Maciej <maciekjur123@gmail.com>
2025-11-03 12:50:40 +0100

    release new version on gh pages

---
+++ My project/Assets/Scripts/Statistics/InfoTooltip.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InfoTooltip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public GameObject infoPanel;
+    
+    [TextArea(3, 10)]
+    public string infoText;
+
+    private TMPro.TextMeshProUGUI textComponent;
+
+    void Start()
+    {
+        if (infoPanel != null)
+        {
+            textComponent = infoPanel.GetComponentInChildren<TMPro.TextMeshProUGUI>();
+            infoPanel.SetActive(false);
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (infoPanel != null && textComponent != null)
+        {
+            textComponent.text = infoText;
+            infoPanel.SetActive(true);
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (infoPanel != null)
+        {
+            infoPanel.SetActive(false);
+        }
+    }
+}
